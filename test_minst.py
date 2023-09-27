a378bf7751df311e9bf659aac474ac25517ac6d6
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-09-27 00:14:43 -0400

    testing mnist using dense nn and starting cnn

---
+++ test_minst.py
@@ -0,0 +1,44 @@
+import numpy as np
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from NeuralNetwork.Dense.dense import Dense
+from Functions.activations import Tanh
+from Functions.losses import mse, mse_prime
+from NeuralNetwork.network import train,predict
+
+def preprocess(x,y,limit):
+    x = x.reshape(x.shape[0], 28*28, 1)
+    x = x.astype("float32") / 255
+
+    y = to_categorical(y)
+    y = y.reshape(y.shape[0], 10, 1)
+
+    return x[:limit], y[:limit]
+
+
+(x_train,y_train), (x_test,y_test) = mnist.load_data()
+
+x_train, y_train = preprocess(x_train, y_train, 2000)
+x_test, y_test = preprocess(x_test, y_test, 400)
+
+network = [
+    Dense(28*28 , 40),
+    Tanh(),
+    Dense(40, 10),
+    Tanh()
+]
+
+train(network, mse, mse_prime, x_train, y_train, epochs=1000, learning_rate=0.01)
+
+test_results=[]
+
+for x,y in zip(x_test,y_test):
+    output = predict(network,x)
+    test_results.append((np.argmax(output),np.argmax(y)))
+correct=0
+for result in test_results:
+    print('prediction:',result[0],'actual:',result[1])
+    if result[0] == result[1]:
+        correct+=1
+
+print("Accuracy of model on test input is:",(correct/len(test_results))*100)
